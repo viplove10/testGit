d5bc8f2fd94d318770478b8001838ecfbd8c6ec4
viplove10 <viplovechaudhary15@gmail.com>
2019-08-12 09:19:06 +0530

    adding new component and ways to use it

--- angular-skl-app/src/app/app.module.ts
+++ angular-skl-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { NewComponent } from './new/new.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    NewComponent
   ],
   imports: [
     BrowserModule
